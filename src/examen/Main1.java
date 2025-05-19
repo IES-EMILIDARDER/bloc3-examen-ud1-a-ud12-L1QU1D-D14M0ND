4f6690ee2f69f90f0828dd915d893a70d5b5b998
L1QU1D-D14M0ND <tomrod04@gmail.com>
2025-05-19 15:57:42 +0200

    tercer commit

--- src/examen/Main1.java
+++ src/examen/Main1.java
@@ -13,6 +13,7 @@ public class Main1 {
     public static void main(String[] args) {
 
         ArrayList<Vehicle> vehicles = new ArrayList<>();
+        ArrayList<Vehicle> filtrat = new ArrayList<>();
         String vehiclesCSV = "\"C:\\temp\\vehicles.csv\"";
 
         try (BufferedReader lector = new BufferedReader(new FileReader(vehiclesCSV));
@@ -24,13 +25,15 @@ public class Main1 {
                     
                     String[] detalles = linea.split(",",5);
                     
-                    //vehicles.add(new Vehicle(detalles[0], detalles[1], detalles[2], (int) detalles[3], (double) detalles[4]));
+                    //vehicles.add(new Vehicle(detalles[0].trim(), detalles[1].trim(), detalles[2].trim(), (int) detalles[3].trim(), (double) detalles[4].trim()));
                     
                 }
             }
         } catch (IOException e) {
             System.err.println("Error al abrir el archivo: " + e.getMessage());
         }
+        
+        vehicles.forEach((e) -> {if (e.any > 2014) {}});
 
     }
 }
--- src/examen/Vehicle.java
+++ src/examen/Vehicle.java
@@ -8,11 +8,53 @@ class Vehicle {
     private double preu;
 
     public Vehicle(String matricula, String marca, String model, int any, double preu) {
+        this.setMatricula(matricula);
+        this.setMarca(marca);
+        this.setModel(model);
+        this.setAny(any);
+        this.setPreu(preu);
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
         this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
         this.marca = marca;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
         this.model = model;
+    }
+
+    public int getAny() {
+        return any;
+    }
+
+    public void setAny(int any) {
         this.any = any;
+    }
+
+    public double getPreu() {
+        return preu;
+    }
+
+    public void setPreu(double preu) {
         this.preu = preu;
     }
+    
+    
 
 }
